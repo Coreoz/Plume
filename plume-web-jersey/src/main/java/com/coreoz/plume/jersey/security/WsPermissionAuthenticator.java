1733b5c42b0dcfcc70ac4355796d183572cdc1b2
Aurélien Manteaux <amanteaux@gmail.com>
2016-08-07 12:35:31 +0200

    Security layer can be used with different authentication systems

--- plume-web-jersey/src/main/java/com/coreoz/plume/jersey/security/WsPermissionAuthenticator.java
+++ plume-web-jersey/src/main/java/com/coreoz/plume/jersey/security/WsPermissionAuthenticator.java
@@ -2,8 +2,6 @@ package com.coreoz.plume.jersey.security;
 
 import java.util.Collection;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import javax.ws.rs.container.ContainerRequestContext;
 
 import org.slf4j.Logger;
@@ -12,14 +10,12 @@ import org.slf4j.LoggerFactory;
 /**
  * Une implémentation de {@link WsAuthenticator} avec le système de permission
  */
-@Singleton
 public class WsPermissionAuthenticator implements WsAuthenticator {
 
 	private static final Logger logger = LoggerFactory.getLogger(WsPermissionAuthenticator.class);
 
 	private final WsRequestPermissionProvider requestPermissionProvider;
 
-	@Inject
 	public WsPermissionAuthenticator(WsRequestPermissionProvider requestPermissionProvider) {
 		this.requestPermissionProvider = requestPermissionProvider;
 	}
--- plume-web-jersey/src/main/java/com/coreoz/plume/jersey/security/WsSecurityFeature.java
+++ plume-web-jersey/src/main/java/com/coreoz/plume/jersey/security/WsSecurityFeature.java
@@ -1,9 +1,10 @@
 package com.coreoz.plume.jersey.security;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.function.Function;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -12,42 +13,59 @@ import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.FeatureContext;
 
 import org.glassfish.jersey.server.internal.LocalizationMessages;
-import org.glassfish.jersey.server.model.AnnotatedMethod;
 
 /**
  * Decrit la fonctionnalité Jersey qui va permettre de vérifier les droits des utilisateurs
  * pour accéder à une ressource/web-service par requête HTTP
+ * @param <A> The annotation type from which a permission will be fetched 
  */
-@Singleton
-public class WsSecurityFeature implements DynamicFeature {
+public class WsSecurityFeature<A extends Annotation> implements DynamicFeature {
 
 	private final WsAuthenticator authenticator;
-
-	@Inject
-	public WsSecurityFeature(WsAuthenticator authenticator) {
+	private final Class<A> permissionAnnotationType;
+	private final Function<A, String> permissionAnnotationExtractor;
+	
+	public WsSecurityFeature(WsAuthenticator authenticator, Class<A> permissionAnnotationType,
+			Function<A, String> permissionAnnotationExtractor) {
 		this.authenticator = authenticator;
+		this.permissionAnnotationType = permissionAnnotationType;
+		this.permissionAnnotationExtractor = permissionAnnotationExtractor;
+	}
+
+	public static WsSecurityFeature<RestrictTo> restictToBased(WsRequestPermissionProvider requestPermissionProvider) {
+		return new WsSecurityFeature<>(
+			new WsPermissionAuthenticator(requestPermissionProvider),
+			RestrictTo.class,
+			RestrictTo::value
+		);
 	}
 
 	@Override
-	public void configure(ResourceInfo resourceInfo, FeatureContext context) {
-		final AnnotatedMethod am = new AnnotatedMethod(resourceInfo.getResourceMethod());
-
+	public void configure(ResourceInfo methodResourceInfo, FeatureContext methodResourcecontext) {
 		// si l'annotation RestrictTo est présente sur la méthode de WS
 		// alors c'est celle-là qui sera utilisée
-		RestrictTo restrictTo = am.getAnnotation(RestrictTo.class);
-		if (restrictTo != null) {
-			context.register(new WsSecurityRequestFilter(restrictTo.value(), authenticator));
-			return;
+		if(!addPermissionFilter(methodResourceInfo.getResourceMethod(), methodResourcecontext)) {
+			// si l'annotation RestrictTo n'est pas présente sur la méthode de WS
+			// mais qu'elle est présente sur la classe
+			// alors la valeur de l'annotation de la classe sera utilisée sur la méthode
+			addPermissionFilter(methodResourceInfo.getResourceClass(), methodResourcecontext);
 		}
-
-		// si l'annotation RestrictTo n'est pas présente sur la méthode de WS
-		// mais qu'elle est présente sur la classe
-		// alors la valeur de l'annotation de la classe sera utilisée sur la méthode
-		restrictTo = resourceInfo.getResourceClass().getAnnotation(RestrictTo.class);
-		if (restrictTo != null) {
-			context.register(new WsSecurityRequestFilter(restrictTo.value(), authenticator));
-			return;
+	}
+	
+	/**
+	 * Add a filter with the appropriate permission to the context if the annotatedElement is annotated with {@link #A}.
+	 * @return true if the filter has been added to the context, else false 
+	 */
+	private boolean addPermissionFilter(AnnotatedElement annotatedElement, FeatureContext methodResourcecontext) {
+		A permissionAnnotation = annotatedElement.getAnnotation(permissionAnnotationType);
+		if (permissionAnnotation != null) {
+			methodResourcecontext.register(new WsSecurityRequestFilter(
+				permissionAnnotationExtractor.apply(permissionAnnotation),
+				authenticator
+			));
+			return true;
 		}
+		return false;
 	}
 
 	private static class WsSecurityRequestFilter implements ContainerRequestFilter {
