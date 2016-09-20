d882ce46236a47e7c6de84111b5643135bcd56da
Aurélien Manteaux <amanteaux@cardiweb.com>
2016-09-20 17:44:40 +0200

    Corrected guice module name

--- plume-services/src/main/java/com/coreoz/plume/guice/GuiceServicesModule.java
+++ plume-services/src/main/java/com/coreoz/plume/guice/GuiceServicesModule.java
@@ -4,7 +4,7 @@ import com.coreoz.plume.services.time.SystemTimeProvider;
 import com.coreoz.plume.services.time.TimeProvider;
 import com.google.inject.AbstractModule;
 
-public class GuiceCoreModule extends AbstractModule {
+public class GuiceServicesModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
