8f1f59697d50108ca0854ecc5ba4c945e20d8d2e
Aurélien Manteaux <amanteaux@cardiweb.com>
2016-09-21 13:06:50 +0200

    Jackson annotation management

--- plume-db-querydsl/src/main/java/com/coreoz/plume/db/querydsl/generation/IdBeanSerializer.java
+++ plume-db-querydsl/src/main/java/com/coreoz/plume/db/querydsl/generation/IdBeanSerializer.java
@@ -1,8 +1,13 @@
 package com.coreoz.plume.db.querydsl.generation;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 
 import com.coreoz.plume.db.querydsl.crud.CrudEntityQuerydsl;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.databind.util.Converter;
 import com.mysema.codegen.CodeWriter;
 import com.mysema.codegen.model.ClassType;
 import com.querydsl.codegen.EntityType;
@@ -13,13 +18,20 @@ import com.querydsl.sql.codegen.ExtendedBeanSerializer;
 
 public class IdBeanSerializer extends ExtendedBeanSerializer {
 
+	private boolean useJacksonAnnotation;
+
 	public IdBeanSerializer() {
 		setPrintSupertype(true);
+
+		this.useJacksonAnnotation = false;
 	}
 
 	@Override
 	public void serialize(EntityType model, SerializerConfig serializerConfig, CodeWriter writer) throws IOException {
 		injectIdInterface(model);
+		if(useJacksonAnnotation) {
+			injectJacksonAnnotation(model);
+		}
 
 		super.serialize(model, serializerConfig, writer);
 	}
@@ -31,6 +43,83 @@ public class IdBeanSerializer extends ExtendedBeanSerializer {
 				break;
 			}
 		}
+	}
+
+
+	protected void injectJacksonAnnotation(EntityType model) {
+		JsonSerialize jacksonAnnotation = new JsonSerialize() {
+			@Override
+			public Class<? extends Annotation> annotationType() {
+				return JsonSerialize.class;
+			}
+
+			@Override
+			public Class<? extends JsonSerializer> using() {
+				return ToStringSerializer.class;
+			}
+
+			@Override
+			public Class<? extends JsonSerializer> contentUsing() {
+				return null;
+			}
+
+			@Override
+			public Class<? extends JsonSerializer> keyUsing() {
+				return null;
+			}
+
+			@Override
+			public Class<? extends JsonSerializer> nullsUsing() {
+				return null;
+			}
+
+			@Override
+			public Class<?> as() {
+				return null;
+			}
+
+			@Override
+			public Class<?> keyAs() {
+				return null;
+			}
+
+			@Override
+			public Class<?> contentAs() {
+				return null;
+			}
+
+			@Override
+			public Typing typing() {
+				return null;
+			}
+
+			@Override
+			public Class<? extends Converter> converter() {
+				return null;
+			}
+
+			@Override
+			public Class<? extends Converter> contentConverter() {
+				return null;
+			}
+
+			@Override
+			public Inclusion include() {
+				return null;
+			}
+
+		};
+
+		for(Property property : model.getProperties()) {
+			if(Long.class.equals(property.getType().getJavaClass())) {
+				property.addAnnotation(jacksonAnnotation);
+			}
+		}
+	}
+
+	public IdBeanSerializer setUseJacksonAnnotation(boolean useJacksonAnnotation) {
+		this.useJacksonAnnotation = useJacksonAnnotation;
+		return this;
 	}
 
 }
