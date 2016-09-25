19e96600b2114f097c3c90dee25938b6675a0233
Aurélien Manteaux <amanteaux@gmail.com>
2016-09-25 19:38:48 +0200

    cleanup

--- plume-db-querydsl/src/test/java/com/coreoz/plume/db/querydsl/DbQuerydslTestModule.java
+++ plume-db-querydsl/src/test/java/com/coreoz/plume/db/querydsl/DbQuerydslTestModule.java
@@ -1,13 +1,10 @@
 package com.coreoz.plume.db.querydsl;
 
-import org.junit.Ignore;
-
 import com.coreoz.plume.conf.guice.GuiceConfModule;
 import com.coreoz.plume.db.guice.DataSourceModule;
 import com.coreoz.plume.db.guice.GuiceDbTestModule;
 import com.google.inject.AbstractModule;
 
-@Ignore
 public class DbQuerydslTestModule extends AbstractModule {
 
 	@Override
