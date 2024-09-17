46d3f4a3fc88847fc73074b5f2deb9cb725f0554
Aurélien Manteaux <amanteaux@coreoz.com>
2024-09-17 12:06:48 +0200

    Fix Sonar issue

--- plume-web-jersey/src/main/java/com/coreoz/plume/jersey/jee/ErrorServlet.java
+++ plume-web-jersey/src/main/java/com/coreoz/plume/jersey/jee/ErrorServlet.java
@@ -1,24 +1,22 @@
 package com.coreoz.plume.jersey.jee;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
+@Slf4j
 public class ErrorServlet extends HttpServlet {
-
-	private static final long serialVersionUID = 6186826368888103404L;
-
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		try(PrintWriter printWriter = resp.getWriter()) {
 			int errorCode = resp.getStatus();
 			String errorMessage = HttpStatus.getStatusText(errorCode);
 			printWriter.println("Error " + errorCode + " : " + errorMessage);
-		}
+		} catch (Exception e) {
+            logger.error("Error displaying error message for error code {}", resp.getStatus());
+        }
 	}
-
 }
