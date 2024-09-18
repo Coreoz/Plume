b7b273cf1be4bc91f8c8f09afdc08c25d55d5306
Aurélien Manteaux <amanteaux@coreoz.com>
2024-09-18 17:49:25 +0200

    Replace org.jetbrains.annotations by javax.annotation

--- plume-web-jersey/src/main/java/com/coreoz/plume/jersey/errors/Validators.java
+++ plume-web-jersey/src/main/java/com/coreoz/plume/jersey/errors/Validators.java
@@ -3,9 +3,9 @@ package com.coreoz.plume.jersey.errors;
 import org.apache.commons.validator.routines.EmailValidator;
 
 import com.google.common.base.Strings;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
 
 /**
@@ -13,42 +13,42 @@ import java.util.List;
  * when the input data does not pass the validator.
  */
 public class Validators {
-	public static<T> @NotNull T checkRequired(@Nullable T parameter) {
+	public static<T> @Nonnull T checkRequired(@Nullable T parameter) {
 		if(parameter == null) {
 			throw new WsException(WsError.FIELD_REQUIRED);
 		}
 		return parameter;
 	}
 
-	public static @NotNull String checkRequired(@NotNull String fieldName, @Nullable String fieldValue) {
+	public static @Nonnull String checkRequired(@Nonnull String fieldName, @Nullable String fieldValue) {
 		if(Strings.isNullOrEmpty(fieldValue)) {
 			throw new WsException(WsError.FIELD_REQUIRED, List.of(fieldName));
 		}
 		return fieldValue;
 	}
 
-	public static<T> @NotNull T checkRequired(@NotNull String fieldName, @Nullable T fieldValue) {
+	public static<T> @Nonnull T checkRequired(@Nonnull String fieldName, @Nullable T fieldValue) {
 		if(fieldValue == null) {
 			throw new WsException(WsError.FIELD_REQUIRED, List.of(fieldName));
 		}
 		return fieldValue;
 	}
 
-	public static @Nullable String checkEmail(@NotNull String fieldName, @Nullable String fieldValue) {
+	public static @Nullable String checkEmail(@Nonnull String fieldName, @Nullable String fieldValue) {
 		if(!EmailValidator.getInstance().isValid(fieldValue)) {
 			throw new WsException(WsError.EMAIL_INVALID, List.of(fieldName));
 		}
 		return fieldValue;
 	}
 
-	public static @Nullable String checkHexaColor(@NotNull String fieldName, @Nullable String fieldValue) {
+	public static @Nullable String checkHexaColor(@Nonnull String fieldName, @Nullable String fieldValue) {
 		if(fieldValue != null && !fieldValue.matches("[0-9a-fA-F]{6}")) {
 			throw new WsException(WsError.COLOR_INVALID, List.of(fieldName));
 		}
 		return fieldValue;
 	}
 
-	public static @Nullable String checkHexaColorWithStartingHash(@NotNull String fieldName, @Nullable String fieldValue) {
+	public static @Nullable String checkHexaColorWithStartingHash(@Nonnull String fieldName, @Nullable String fieldValue) {
 		if(fieldValue != null) {
             if (!fieldValue.startsWith("#")) {
                 throw new WsException(WsError.COLOR_INVALID, List.of(fieldName));
